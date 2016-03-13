9d1395653da34829962c6c5894090c8f3fee6ff2
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2016-03-13 18:23:07 +0100

    Setup page

--- out/backend/main.js
+++
@@ -1,6 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOlsibWFpbi5jb2ZmZWUiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
